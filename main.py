4cf48b8afc4258030ca3791751c6fdab36881112
JC-C4B <136736016+JC-C4B@users.noreply.github.com>
2024-07-24 13:38:12 -0400

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,21 @@
+# Importing our web app library
+import streamlit as st
+
+# Adjusting the layout of our webpage to look better
+st.set_page_config(layout="wide")
+
+# Creating two columns to organize our information
+col1, col2 = st.columns(2)
+
+# Displaying our Photo
+with col1:
+    st.image("images/PHOTO.jpg", width=300)
+
+# Display our initial text
+with col2:
+    st.title("Juan Carlos Cabrera")
+    content = """
+    Hello! I'm Juan Carlos. I'm a recent Software Engineering graduate aiming to leverage my abilities to successfully become a reliable member of a software engineering team. 
+    I look forward to potentially working with you!
+"""
+    st.info(content)
