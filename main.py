22548181d2dd404b5bb3aef146923c72f2c3f172
JC-C4B <136736016+JC-C4B@users.noreply.github.com>
2024-07-25 12:01:31 -0400

    Added description, image, and links

--- main.py
+++ main.py
@@ -32,16 +32,23 @@ Feel free to contact me!
 st.write(content2)
 
 # Project columns
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 # Getting data from csv
 df = pandas.read_csv("data.csv", sep=";")
 
-# 
+# First project display column
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
+# Second project display column
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
