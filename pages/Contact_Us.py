20a25913a4e9e532a3dca7302adaf0f8801a3e3c
JC-C4B <136736016+JC-C4B@users.noreply.github.com>
2024-12-03 12:33:11 -0500

    Topics selection menu added to contact form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,15 +1,23 @@
 import streamlit as st
 from send_email import send_email
+import pandas
+
+df = pandas.read_csv("options.csv")
 
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
+    option = st.selectbox(
+        "What would you like to discuss?",
+        df["Options"],)
     raw_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+
+Topic: {option}
 
 {raw_message}
 """
--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "jcxc4b@gmail.com"
-    password = "sxgr vjna tkds llxf"
+    password = os.getenv("PASSWORD")
 
     receiver = "jcxc4b@gmail.com"
     my_context = ssl.create_default_context()
